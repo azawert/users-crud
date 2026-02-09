d41ebb84b55db592bd1d20ba171525ee92ab16e0
azawert <53710085+azawert@users.noreply.github.com>
2026-02-09 23:20:48 +0300

    chore: :bug: biome config update

--- src/auth/jwt-access.strategy.ts
+++ src/auth/jwt-access.strategy.ts
@@ -1,28 +1,28 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { PassportStrategy } from '@nestjs/passport';
-import { ExtractJwt, Strategy } from 'passport-jwt';
-import { ACCESS_TOKEN_SECRET_KEY, JWT_ACCESS_STRATEGY_NAME } from 'src/common';
-import type User from 'src/user/user.entity';
-import { UserService } from 'src/user/user.service';
+import { Injectable, UnauthorizedException } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { PassportStrategy } from '@nestjs/passport'
+import { ExtractJwt, Strategy } from 'passport-jwt'
+import { ACCESS_TOKEN_SECRET_KEY, JWT_ACCESS_STRATEGY_NAME } from 'src/common'
+import type User from 'src/user/user.entity'
+import { UserService } from 'src/user/user.service'
 @Injectable()
 export class JwtAccessStrategy extends PassportStrategy(Strategy, JWT_ACCESS_STRATEGY_NAME) {
-	constructor(
-		private readonly userService: UserService,
-		private readonly configService: ConfigService
-	) {
-		super({
-			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-			secretOrKey: configService.get(ACCESS_TOKEN_SECRET_KEY)!,
-		});
-	}
-	async validate(payload: any): Promise<User> {
-		const { sub: userId } = payload;
-		const user = await this.userService.getById(userId);
+  constructor(
+    private readonly userService: UserService,
+    private readonly configService: ConfigService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: configService.get(ACCESS_TOKEN_SECRET_KEY)!,
+    })
+  }
+  async validate(payload: any): Promise<User> {
+    const { sub: userId } = payload
+    const user = await this.userService.getById(userId)
 
-		if (!user) {
-			throw new UnauthorizedException();
-		}
-		return user;
-	}
+    if (!user) {
+      throw new UnauthorizedException()
+    }
+    return user
+  }
 }
